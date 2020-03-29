e95cfff1913139e45d69787e3a2a745158cfa498
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2020-03-29 16:08:53 +0200

    fix function name

--- src/chronodrive.js
+++ src/chronodrive.js
@@ -43,7 +43,7 @@ async function crawlChronodrive () {
 
     winston.info(`crawlChronodrive;${message}`)
 
-    if (['Pas de créneau disponible', 'Pas de créneaux disponibles'].include(message) === false ) {
+    if (['Pas de créneau disponible', 'Pas de créneaux disponibles'].includes(message) === false ) {
       const options = {
         host: process.env.MAILER_HOST,
         port: process.env.MAILER_PORT,
